851733fa76b9cd852057da5f2ddc7adae70aa7f8
Arseniy <arseniy.karmanov@mail.ru>
2020-10-12 13:05:09 +0900

    markupApp1 completed

--- app/src/main/java/com/karmanchik/markupapp1/MainActivity.java
+++ app/src/main/java/com/karmanchik/markupapp1/MainActivity.java
@@ -3,12 +3,46 @@ package com.karmanchik.markupapp1;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
+    private EditText editText;
+    private Button sendButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        editText = findViewById(R.id.editTextTextPersonNameId);
+        sendButton = findViewById(R.id.sendButtonId);
+
+        sendButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(editText.getText() != null)
+                    editText.setText("");
+            }
+        });
+    }
+
+
+
+    public EditText getEditText() {
+        return editText;
+    }
+
+    public void setEditText(EditText editText) {
+        this.editText = editText;
+    }
+
+    public Button getSendButton() {
+        return sendButton;
+    }
+
+    public void setSendButton(Button sendButton) {
+        this.sendButton = sendButton;
     }
 }
